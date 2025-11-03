b4c30b8a3bac0f66a9f7b639a4dcb95adb3ac9dd
Robert Rowlands <ROB2107834@maricopa.edu>
2025-11-02 19:36:01 -0700

    Broke the testing into 2 seperate functions. Tests passed. no frills game. no frills tests. Minimum Viable Product.

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -4,13 +4,36 @@
 
 # Testing using pytest
 
-from guess_the_word import word_list, word_rand
+
+# importing things to test
+from guess_the_word import word_list, word_rand, hide_word
 
 # test if word is from list
-def test_guess_the_word():
+def test_the_word():
+
+    # Test 1 is word in word list
     for i in range(20):
         chosen_word = word_rand(word_list)
         assert chosen_word in word_list
+
+
+    # Test 2 do the guesses work right
+def test_the_guess():
+    the_word = "ZOMBIES"
+
+    hidden_word = hide_word(the_word, {"Z", "O"})
+    assert hidden_word == "ZO_____"
+
+    hidden_word = hide_word(the_word, {"Z", "Y"})
+    assert hidden_word == "Z______"
+
+    hidden_word = hide_word(the_word, {"A", "C"})
+    assert hidden_word == "_______"
+    
+
+    
+
+
 
 
 
